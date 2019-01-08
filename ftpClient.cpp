a986a0c9f41f92a1251015d3424f2dd831714c81
Tyler <tylerjreiland@gmail.com>
2019-01-07 21:20:51 -0600

    Server and Client can communicate

--- ftpClient.cpp
+++ ftpClient.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 
 int main( int argc, char* argv[] )
 {
 
-	/* GNU GETOPT  */ 
+	/* GNU GETOPT
 	int opt;
 	while ( (opt = getopt(argc, argv, "") ) != -1)
 	{
@@ -19,7 +24,44 @@ int main( int argc, char* argv[] )
 				 printf("%c is not an option", opt); 
 				 break; 
 		}
+	}*/
+	try
+    {
+    	if (argc != 2)
+	    {
+	      	std::cerr << "Incorrect Usage, Use: ./client <host>" << std::endl;
+	      	return 1;
+	    }
+
+	    boost::asio::io_context io_context;
+
+	    tcp::resolver resolver(io_context);
+	    tcp::resolver::results_type endpoints =
+	      resolver.resolve(argv[1], "daytime");
+
+	    tcp::socket socket(io_context);
+	    boost::asio::connect(socket, endpoints);
+
+	    for (;;)
+	    {
+		    boost::array<char, 128> buf;
+		    boost::system::error_code error;
+
+		    size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+		    if (error == boost::asio::error::eof)
+		      break; // Connection closed cleanly by peer.
+		    else if (error)
+		      throw boost::system::system_error(error); // Some other error.
+
+		  	printf("NEW MESSAGE FROM SERVER: %s", buf.data());
+		    //std::cout.write(buf.data(), len);
+	    }
 	}
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
 	return 0;
 
 }
